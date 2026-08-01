8f8d6a894244372d7537daa58e1f869608760b27
Enrique Andreazza <enrique0801@gmail.com>
2026-08-01 17:34:49 -0300

    Configura projeto React + Vite com Home page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main id="home">
+      <h1>RPG MAPS</h1>
+      <div id="menu">
+        <button>Iniciar</button>
+        <button>Configurações</button>
+      </div>
+    </main>
+  )
+}
+
+export default App
